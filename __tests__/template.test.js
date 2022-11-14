bdd4222ff8049d54db95fedf43837794efc84b19
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-14 08:31:53 -0800

    Add tests for function isPositiveNumber

--- __tests__/template.test.js
+++ __tests__/template.test.js
@@ -1,3 +1,24 @@
-import { NAMED_FUNCTION } from '../src/js/template.js';
+import { isPositiveNumber } from '../src/js/template.js';
 
-describe('', () => {})
+describe('isPositiveNumber', () => {
+
+  test('It should return false if NaN', (number) => {
+    const number = "#";
+    expect(validateNumber(number)).toEqual(false);
+  });
+
+  test('It should return false if negative number', (number) => {
+    const number = "-1";
+    expect(validateNumber(number)).toEqual(false);
+  });
+
+  test('It should return true if positive number', (number) => {
+    const number = "36";
+    expect(validateNumber(number)).toEqual(true);
+  });
+
+});
+
+describe('Age', () => {
+  // Create a class containing person's age
+})
