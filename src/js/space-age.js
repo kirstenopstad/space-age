616064db9476da791b4fd913830e1a9cebf5164d
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-14 09:42:33 -0800

    Refactor to use roundToTwo in methods of Age

--- src/js/space-age.js
+++ src/js/space-age.js
@@ -23,18 +23,16 @@ export class Age {
 
   // A Mercury year is .24 Earth years.
   getMercuryAge() {
-    return this.mercuryAge = this.earthAge * .24 
+    return roundToTwo(this.mercuryAge = this.earthAge * .24)
   }
 
   // A Venus year is .62 Earth years.
   getVenusAge() {
-    return this.venusAge = this.earthAge * .62
+    return roundToTwo(this.venusAge = this.earthAge * .62)
   }
 
   // A Mars year is 1.88 Earth years.
   getMarsAge() {
-    this.marsAge = this.earthAge * 1.88
-    this.marsAge = parseFloat(Number.parseFloat(this.marsAge).toFixed(2))
-    return this.marsAge
+    return roundToTwo(this.marsAge = this.earthAge * 1.88)
   }
 }
