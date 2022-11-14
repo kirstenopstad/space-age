11dbabc073cf754aade939b71106579383598a6b
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-14 08:58:17 -0800

    Passes tests for isPositiveNumber & Age constructor

---
+++ src/js/space-age.js
@@ -0,0 +1,18 @@
+// Business Logic
+
+// Validate input
+export function isPositiveNumber(number) {
+  if (parseInt(number) != number) {
+    return false;
+  } else if (parseInt(number) <= 0) {
+    return false;
+  } else {
+    return true;
+  };
+}
+
+export class Age {
+  constructor(age){
+    this.earthAge = age;
+  }
+}
