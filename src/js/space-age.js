227ca91363159ed52029255719ad3febd7e3cc81
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-14 09:30:12 -0800

    Passes test 'should return age on Mars'

--- src/js/space-age.js
+++ src/js/space-age.js
@@ -16,7 +16,20 @@ export class Age {
     this.earthAge = age;
   }
 
+  // A Mercury year is .24 Earth years.
   getMercuryAge() {
     return this.mercuryAge = this.earthAge * .24 
+  }
+
+  // A Venus year is .62 Earth years.
+  getVenusAge() {
+    return this.venusAge = this.earthAge * .62
+  }
+
+  // A Mars year is 1.88 Earth years.
+  getMarsAge() {
+    this.marsAge = this.earthAge * 1.88
+    this.marsAge = parseFloat(Number.parseFloat(this.marsAge).toFixed(2))
+    return this.marsAge
   }
 }
