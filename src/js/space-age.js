57f10d68bae80c6f9aca2658437a21435db0431a
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-14 09:57:04 -0800

    Refactor code to ensure rounded value stored in object

--- src/js/space-age.js
+++ src/js/space-age.js
@@ -23,17 +23,17 @@ export class Age {
 
   // A Mercury year is .24 Earth years.
   getMercuryAge() {
-    return roundToTwo(this.mercuryAge = this.earthAge * .24)
+    return this.mercuryAge = roundToTwo(this.earthAge * .24)
   }
 
   // A Venus year is .62 Earth years.
   getVenusAge() {
-    return roundToTwo(this.venusAge = this.earthAge * .62)
+    return this.venusAge = roundToTwo(this.earthAge * .62)
   }
 
   // A Mars year is 1.88 Earth years.
   getMarsAge() {
-    return roundToTwo(this.marsAge = this.earthAge * 1.88)
+    return this.marsAge = roundToTwo(this.earthAge * 1.88)
   }
 
   // A Jupiter year is 11.86 Earth years.
