e5306f6bd819d31a4fc17a9801707896c9ba47a6
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-14 10:31:45 -0800

    Add test to calculate life expectancy on earth

--- __tests__/space-age.test.js
+++ __tests__/space-age.test.js
@@ -36,13 +36,22 @@ describe('roundToTwo', () => {
 describe('Age', () => {
   let age;
   let spaceAges;
+  let expectedYearsLeft;
   beforeEach(() => { 
     age = new Age(36);
+    // Space ages
     spaceAges = new Age(36);
     spaceAges.mercuryAge = 8.64;
     spaceAges.venusAge = 22.32;
     spaceAges.marsAge = 67.68;
     spaceAges.jupiterAge = 426.96;
+    // Life expectancies (planetAge - planetYears)
+    expectedYearsLeft = new Age(36);
+    expectedYearsLeft.earthYears = 79 - age.earthAge;
+    expectedYearsLeft.mercuryYears = 18.96 - spaceAges.mercuryAge;
+    expectedYearsLeft.venusYears = 48.98 - spaceAges.venusAge;
+    expectedYearsLeft.marsYears = 148.52 - spaceAges.marsAge;
+    expectedYearsLeft.jupiterYears = 936.94 - spaceAges.jupiterAge;
     });
 
   test('It should create a class containing a persons age', () => {
@@ -72,4 +81,9 @@ describe('Age', () => {
     age.getJupiterAge();
     expect(age).toMatchObject(spaceAges);
   })
+  
+  test('It should calculate earth years left', () => {
+    expect(age.getEarthExpectancy(79)).toEqual(expectedYearsLeft.earthYears);
+  });
+
 });
