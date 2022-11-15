84db1fd584abbfab3e200cb8597e2e4906c990cc
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-14 16:53:22 -0800

    Add css styling to add gray bg color

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { isPositiveNumber, Age } from '../src/js/space-age.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import './css/styles.css';
 
 // UI Logic
 
